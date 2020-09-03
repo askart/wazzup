0757956ef7b91fc4b5a74997b07c5060edd6872f
askart <tulebaev.askar@gmail.com>
2020-09-03 13:33:03 +0600

    mock response for production

--- src/api/users.js
+++ src/api/users.js
@@ -1,8 +1,14 @@
 import http from "@/services/http"
+const rows = require("response.json")
 
 export function get() {
-  const path = "?rows=1000&id={index}&fullname={firstName}~{lastName}&company={business}&email={email}&uname={username}&address={addressObject}"
-  return http.get(path)
+  if (process.env.NODE_ENV == "production") {
+    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+    return wait(1 * 1000).then(() => rows)
+  } else {
+    const path = "?rows=1000&id={index}&fullname={firstName}~{lastName}&company={business}&email={email}&uname={username}&address={addressObject}"
+    return http.get(path)
+  }
 }
 
 
